e49fae34d9e86f209e5b081434bee27d502136ba
John Park <johnpark.cs@gmail.com>
2025-05-08 23:46:13 -0500

    Removed repository package and unused EventRepository

--- src/main/java/com/yohan/yohan_planner/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yohan.yohan_planner.repository;
-
-import com.yohan.yohan_planner.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
